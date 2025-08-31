7196efffc301a5e9d1a65a3462ec0534ea23541f
TestaMic <mateidiac9@gmail.com>
2025-08-31 11:36:10 +0300

    sm7125-common Re-Enable Flash intensity support.
    This reverts commit 160967fd1ca9c19509519adf8e17356a38c0f280.
    
    Revert "Revert "Update BoardConfigCommon.mk - Add Samsung ext SePolicy""
    
    This reverts commit a785beea4af7e119446ced26435c1ecdf6820800.
    
    Revert "Revert "Update common.mk - Add flash control""
    
    This reverts commit 429ab8dc38701017d0f224618103496d098e7e1f.
    
    Revert "Revert "Update common.mk - Add samsung ext soong namespace""
    
    This reverts commit f0e41a9b0462b9647235195fe798a9d59bc22308.
    
    Revert "Revert "Create hal_samsung_camera_flashlight_default.te - Setup SePolicy""
    
    This reverts commit 00fbc290895d22c55820b5a9792c710cd3d0ff6b.
    
    Revert "Revert "Update file.te - Setup Sepolicy""
    
    This reverts commit f26337a52a01cb4090442e756418e6d0890f3ddd.

--- common.mk
+++ common.mk
@@ -382,6 +382,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
+#Flash control
+PRODUCT_PACKAGES += \
+    vendor.samsung_ext.hardware.camera.flashlight-service \
+    FlashControl
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
@@ -433,6 +438,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/samsung-ext/interfaces \
     hardware/samsung
 
 # Prop files
