70e8930eff90a381355bd66f4c07e55c2346f184
TestaMic <mateidiac9@gmail.com>
2025-08-31 11:50:28 +0300

    sm7125-common: Move Lineage Health HAL to select() and remove forgotten Init declaration.

--- common.mk
+++ common.mk
@@ -207,6 +207,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.samsung-recovery \
     android.hardware.health@2.1.vendor
 
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
