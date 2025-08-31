429ab8dc38701017d0f224618103496d098e7e1f
TestaMic <mateidiac9@gmail.com>
2025-08-31 10:58:19 +0300

    Revert "Update common.mk - Add flash control"
    This reverts commit 5ac480eda64e2ee1beef4ceb3a5cf393480ae796.

--- common.mk
+++ common.mk
@@ -388,11 +388,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
-#Flash control
-PRODUCT_PACKAGES += \
-    vendor.samsung_ext.hardware.camera.flashlight-service \
-    FlashControl
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
