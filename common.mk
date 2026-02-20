06276a6399d439dad46a9802b62519899a9d1e2e
TestaMic <mateidiac9@gmail.com>
2026-02-20 21:51:46 +0200

    sm7125-common: Remove duplicate charging control proprieties.

--- common.mk
+++ common.mk
@@ -214,10 +214,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.samsung-recovery \
     android.hardware.health@2.1.vendor
 
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/input_suspend)
-
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
